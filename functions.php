611f56fe42505a936e15245396d3f955d301defe
wolf2234 <viktoral9599@gmail.com>
2025-02-18 05:52:26 +0300

    this commit adds header and footer blocks.

--- functions.php
+++ functions.php
@@ -1,6 +1,26 @@
 <?php 
 add_action('wp_enqueue_scripts', 'event_calendar_styles');
 add_action('wp_enqueue_scripts', 'event_calendar_scripts');
+add_action('after_setup_theme', 'event_calendar_nav_menu');
+add_theme_support('custom-logo');
+
+
+function event_calendar_nav_menu() {
+    register_nav_menu( 'top', 'menu in header' );
+    register_nav_menu( 'bottom', 'menu in footer' );
+}
+
+
+add_filter('single_template', function($template) {
+    if (get_post_type() == 'event_listing') {
+        $new_template = locate_template(array('custom-event-template.php'));
+        if (!empty($new_template)) {
+            return $new_template;
+        }
+    }
+    return $template;
+});
+
 
 
 function event_calendar_styles() {
@@ -16,3 +36,39 @@ function event_calendar_scripts() {
 }
 
 
+add_action( 'event_manager_event_filters_search_events_end', 'filter_by_country_field' );
+function filter_by_country_field() { ?>
+<div class="wpem-row">
+    <div class="wpem-col">
+        <div class="wpem-form-group">
+                <div class="search_event_types">
+                <label for="search_event_types" class="wpem-form-label"><?php _e( 'Country', 'event_manager' ); ?></label>
+                <select name="filter_by_country" class="event-manager-filter">
+                    <option value=""><?php _e( 'Select country', 'event_manager' ); ?></option>
+                    <option value="de"><?php _e( 'Germany', 'event_manager' ); ?></option>
+                    <option value="in"><?php _e( 'India', 'event_manager' ); ?></option>
+                    <option value="us"><?php _e( 'USA', 'event_manager' ); ?></option>
+                </select>
+            </div>
+        </div>
+    </div>
+</div>
+<?php 
+}
+
+
+add_filter( 'event_manager_get_listings', 'filter_by_country_field_query_args', 10, 2 );
+function filter_by_country_field_query_args( $query_args, $args ) {
+    if ( isset( $_POST['form_data'] ) ) {
+        parse_str( $_POST['form_data'], $form_data );
+        // If this is set, we are filtering by country
+        if ( ! empty( $form_data['filter_by_country'] ) ) {
+            $event_country = sanitize_text_field( $form_data['filter_by_country'] );
+            $query_args['meta_query'][] = array(
+                        'key'     => '_event_country',
+                        'value'   => $event_country,
+                        );
+        }
+    }
+    return $query_args;
+}
--- index.php
+++ index.php
@@ -7,10 +7,81 @@
     <title><?php bloginfo('name'); ?></title>
 </head>
 <body>
-        <div class="container">
+    <div class="wrapper">
+        <header class="header">
+            <div class="container-large">
+                <div class="head">
+                        <?php the_custom_logo($blog_id); ?>
+                        <nav class="nav-menu">
+                            <?php wp_nav_menu(array(
+                            'theme_location' => 'top', 
+                            'menu' => 'nav-menu',
+                            'container' => null,
+                            'menu_class' => 'menu',
+                            )); 
+                            ?>
+                        </nav>
+                </div>
+            </div>
+        </header>
+        <div class="container-small">
             <main class="main">
+                <?php echo do_shortcode('[events]'); ?>
             </main>
         </div>
+        <footer class="footer">
+            <div class="footer__body">
+                <div class="container-large">
+                    <div class="info">
+                        <div class="contact">
+                            <?php the_custom_logo($blog_id); ?>
+                            <div class="contact__info">
+                                <a href="#" class="contact__email">
+                                    <img src="<?php the_field('email_icon'); ?>" alt="">
+                                    <?php the_field('email'); ?>
+                                </a>
+                                <a href="#" class="contact__address">
+                                    <img src="<?php the_field('address_icon'); ?>" alt="">
+                                    <?php the_field('address'); ?>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="learn-more">
+                            <h3 class="learn-more__title">Learn more</h3>
+                            <?php wp_nav_menu(array(
+                                'theme_location' => 'bottom', 
+                                'menu' => 'learn-more',
+                                'container' => null,
+                                'menu_class' => 'learn-more__links',
+                                )); 
+                                ?>
+                        </div>
+                        <div class="social">
+                            <h3 class="social__title">Social links</h3>
+                            <div class="social__links">
+                                <a class="social__link" href="https://www.spacex.com/">
+                                    <img src="<?php the_field('space_x'); ?>" alt="">
+                                </a>
+                                <a class="social__link" href="https://www.instagram.com/">
+                                    <img src="<?php the_field('instagram'); ?>" alt="">
+                                </a>
+                                <a class="social__link" href="https://www.facebook.com/">
+                                    <img src="<?php the_field('facebook'); ?>" alt="">
+                                </a>
+                                <a class="social__link" href="https://www.linkedin.com/">
+                                    <img src="<?php the_field('linkedin'); ?>" alt="">
+                                </a>
+                                <a class="social__link" href="https://www.whatsapp.com">
+                                    <img src="<?php the_field('whatsapp'); ?>" alt="">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p class="copyright">Copyright © 2022-2029 example.com. All Rights Reserved.</p>
+        </footer>
+    </div>
     <?php wp_footer(); ?>
 </body>
 
