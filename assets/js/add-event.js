aaed07021f1ccd2e1c1c4f679dcc6c87e8ccc87d
wolf2234 <viktoral9599@gmail.com>
2025-03-05 13:09:39 +0300

    this commit adds delegation with body for multi-field__plus.

--- assets/js/add-event.js
+++ assets/js/add-event.js
@@ -1,30 +1,27 @@
-// document.addEventListener("DOMContentLoaded", function () {
-document
-    .querySelector(".event-form__btn")
-    .addEventListener("click", function (event) {
-        event.preventDefault();
-        let fileInputs = document.querySelectorAll(".multi-field__input");
-        document.querySelectorAll(".multi-field__radio").forEach((mark) => {
-            let radio = mark.getElementsByTagName("input")[0];
-            console.log(radio.checked, "Radio", radio.id);
+document.addEventListener("DOMContentLoaded", function () {
+    document
+        .querySelector(".event-form__btn")
+        .addEventListener("click", function (event) {
+            event.preventDefault();
+            let fileInputs = document.querySelectorAll(".multi-field__input");
+            document.querySelectorAll(".multi-field__radio").forEach((mark) => {
+                let radio = mark.getElementsByTagName("input")[0];
+                console.log(radio.checked, "Radio", radio.id);
+            });
+            fileInputs.forEach((fileInput) => {
+                let file = fileInput.files[0];
+                if (file) {
+                    let reader = new FileReader();
+                    reader.onload = function (e) {};
+                    reader.readAsDataURL(file);
+                }
+                console.log(file);
+            });
         });
-        fileInputs.forEach((fileInput) => {
-            let file = fileInput.files[0];
-            if (file) {
-                let reader = new FileReader();
-                reader.onload = function (e) {};
-                reader.readAsDataURL(file);
-            }
-            console.log(file);
-        });
-    });
-addMultiField();
-let ff = document.querySelectorAll(".multi-field__plus");
-ff.forEach((plus) => {
-    console.log(document.querySelectorAll(".multi-field__plus").length);
-    plus.addEventListener("click", function (e) {
-        createMultiField();
-        console.log(document.querySelectorAll(".multi-field").length, "Plus");
+    addMultiField();
+    document.body.addEventListener("click", function (e) {
+        if (e.target.classList.contains("multi-field__plus")) {
+            createMultiField();
+        }
     });
 });
-// });
