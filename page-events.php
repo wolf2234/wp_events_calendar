08e04949823632bd1c6e93675822bd72522d236b
wolf2234 <viktoral9599@gmail.com>
2025-03-20 04:01:51 +0300

    this commit fixes bug on events page.

--- page-events.php
+++ page-events.php
@@ -6,15 +6,13 @@ wp_head();
 ?>
 <div class="wrapper">
     <?php include 'includes/header.php';?>
-    <div class="event-info">
-        <div class="title">
-            <h1 class="title__name">Events</h1>
-        </div>
-        <div class="container-large events">
+    <div class="title">
+        <h1 class="title__name">Events</h1>
+    </div>
+    <div class="events">
+        <div class="events">
             <div class="events__body">
-                <div class="events__items">
-                    
-                </div>
+                <div class="events__items"></div>
             </div>
         </div>
     </div>
